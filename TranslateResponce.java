ee5d50c11a3774dbc8688bfd14637809fc5b00ea
Alenafox <57696470+Alenafox@users.noreply.github.com>
2020-11-11 22:09:34 +0800

    Create TranslateResponce.java

---
+++ TranslateResponce.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+class TranslateResponce {
+    ArrayList<Translations> translations;
+}
+
+class Translations {
+    String text;
+}
